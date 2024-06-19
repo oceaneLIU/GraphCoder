0f11fd24db113e17a9f66c4981e7e98e06c435f1
OceaneLIU <weiliu.oceane@gmail.com>
2024-06-19 17:48:04 +0800

    update build query graph

--- build_query_graph.py
+++ build_query_graph.py
@@ -33,7 +33,7 @@ def build_query_subgraph(task_name):
             with open(os.path.join(CONSTANTS.repo_base_dir, case_path), 'r') as f:
                 src_lines = f.readlines()
             query_context = src_lines[:line_no]
-            ccg = create_graph(query_context)
+            ccg = create_graph(query_context, case_id)
             query_ctx, query_line_list, query_graph = last_n_context_lines_graph(ccg)
             graph_case = dict()
             graph_case['query_forward_graph'] = graph_to_json(query_graph)
